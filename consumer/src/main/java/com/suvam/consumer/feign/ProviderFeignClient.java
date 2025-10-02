ed69f22942f00d4983bf337e63a67b68d3ba3100
Suvam Debnath <debnathsuvam322@gmail.com>
2025-10-02 14:41:36 +0530

    used @LoadBalanced with OpenFeign for declarative API calls

--- consumer/src/main/java/com/suvam/consumer/feign/ProviderFeignClient.java
+++ consumer/src/main/java/com/suvam/consumer/feign/ProviderFeignClient.java
@@ -3,8 +3,10 @@ package com.suvam.consumer.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="provider-service", url="http://localhost:8082")
+//@FeignClient(name="provider-service", url="http://localhost:8082")
+@FeignClient(name="provider")
 public interface ProviderFeignClient {
+
     @GetMapping("/instance-info")
     String getInstanceInfo();
 }
--- provider/src/main/java/com/suvam/provider/ProviderApplication.java
+++ provider/src/main/java/com/suvam/provider/ProviderApplication.java
@@ -2,6 +2,7 @@ package com.suvam.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ProviderApplication {
