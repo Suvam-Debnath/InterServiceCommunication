f6adc25413dccfaa5fa8d03d0e8fcfb4e0e25e50
Suvam Debnath <debnathsuvam322@gmail.com>
2025-10-03 07:18:49 +0530

    @LoadBalanced with HTTP Interface

--- consumer/src/main/java/com/suvam/consumer/httpinterface/HttpInterfaceConfig.java
+++ consumer/src/main/java/com/suvam/consumer/httpinterface/HttpInterfaceConfig.java
@@ -1,16 +1,26 @@
 package com.suvam.consumer.httpinterface;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class HttpInterfaceConfig {
+
     @Bean
-    public ProviderHttpInterface webClientHttpInterface(){
-        WebClient webClient = WebClient.builder().baseUrl("http://localhost:8082").build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
+    }
+
+    @Bean
+    public ProviderHttpInterface webClientHttpInterface(WebClient.Builder webClientBuilder){
+        //WebClient webClient = WebClient.builder().baseUrl("http://localhost:8082").build();
+        WebClient webClient = webClientBuilder.baseUrl("http://provider").build();
         WebClientAdapter adapter = WebClientAdapter.create(webClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         ProviderHttpInterface service = factory.createClient(ProviderHttpInterface.class);
